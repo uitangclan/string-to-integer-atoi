b0e6b8752c043269adbfa4e861a0bf448bceccd9
uitangclan <uitangclan@gmail.com>
2024-10-14 17:26:20 +0900

    happy coding

--- MyAtoi.kt
+++ MyAtoi.kt
@@ -1,54 +1,94 @@
 fun main() {
     // var s = "1337c0d3"
     // var s = "words and 987"
-    // var s = "+-12"
+    // var s = "  -457 +hello"
     // var s = ""
     // var s = "  12    3   4   "
-    var s = "00123"
-    myAtoi(s)
+    var s = "2147483648"
+    println(myAtoi(s))
 }
 
-fun myAtoi(s: String): Int {
-    var sign = ' '
-    var start = 0
-    var end = 0
-    var str = s.trim()
-    var i = str.length
-    
-    if(str.isNullOrEmpty()){
-        return 0
+enum class State {
+    born,
+    again,
+    survived,
+    dead
+}
+
+class StateMachine {
+    var currrentState = State.born
+    var sign = 1
+    var result = 0
+
+    fun toAgain(ch: Char) {
+        sign = if (ch == '-') -1 else 1
+        currrentState = State.again
     }
     
-    if(str[0] == '-' || str[0] == '+') {
-        sign = str[0]
-        start = 1
-        end = 1
-    } else if (!str[0].isDigit()) {
-        return 0
+    fun toSurvived(digit: Int) {
+        
+        currrentState = State.survived
+        appendDigit(digit)
     }
     
-    while(end < i && str[end].isDigit()){
-        end++
-    }
-    
-    if (start == end) {
-        return 0
+    fun toDead() {
+        currrentState = State.dead
     }
 
-
-    var result = try {
-        str.substring(start,end).toInt()
-    } catch (e: Exception) {
-        if (sign == '-') {
-            return Integer.MIN_VALUE
+    fun appendDigit(digit: Int) {
+        if(Int.MAX_VALUE / 10 < result || (
+            Int.MAX_VALUE == result && Int.MAX_VALUE % 10 < digit)) {
+                if(sign == 1) {
+                    result = Int.MAX_VALUE
+                } else {
+                    result = Int.MIN_VALUE
+                    sign = 1
+                }
+                toDead()
         } else {
-            return Integer.MAX_VALUE
+            result = result * 10 + digit
+
+        }
+
+    }
+
+    fun transit(ch: Char) {
+        when(currrentState) {
+            State.born -> {
+                when {
+                    ch == ' ' -> {
+
+                    }
+                    ch == '+' || ch == '-' -> {
+                        toAgain(ch)
+                    }
+                    ch.isDigit() -> {
+                        toSurvived(ch - '0')
+                    }
+                    else -> {
+                        toDead()
+                    }
+                }
+            }
+            State.again, State.survived -> {
+                if (ch.isDigit()) {
+                    toSurvived(ch - '0')                    
+                } else {
+                    toDead()
+                }
+            }
+            else -> Unit
         }
     }
-    if (sign == '-') {
-        result = -result
-    }
+}
 
-    println(result)
-    return result
+
+fun myAtoi(input: String): Int {
+    val sm = StateMachine()
+
+    for (ch in input) {
+        if(sm.currrentState == State.dead) break
+            sm.transit(ch)
+    }
+    return sm.sign * sm.result
 }
